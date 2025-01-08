71ad656a7312c8d6d800d8ad62adbd29f74dd2b0
Torgallo68 <145479518+Torgallo68@users.noreply.github.com>
2025-01-08 14:14:36 +0100

    Update settings.py

--- PotegaApp/backend/PotegaApp/settings.py
+++ PotegaApp/backend/PotegaApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&aml(ko1l$ofc@uldx7x*4mujs^09b(ro3qkidmd7f@%)=jyek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
